6f6ef8a7019038ae27389700bd33882c4b3d14ba
jmbeach <jaredbeachdesign@gmail.com>
2021-11-06 18:33:49 -0500

    far far far

---
+++ far-further.ts
@@ -0,0 +1,27 @@
+import * as fs from 'fs';
+import * as hsl from 'hsl-to-hex';
+
+const farNearer = fs.readFileSync('far nearer_template.svg', 'utf-8');
+const hue1 = 305
+const color1SL = [100, 50]
+const hue2 = 337
+const color2SL = [100, 49]
+const hue3 = 26
+const color3SL = [98, 68]
+
+const adjustColor = (color: number, amount: number) => {
+  let result = color + amount;
+  if (result > 360) { result = 360 - result }
+  if (result < 0) { result = 360 + result }
+  return result;
+}
+
+for (let i = 0; i < 360; i++) {
+  const newColor1 = hsl(adjustColor(hue1, i), color1SL[0], color1SL[1]);
+  const newColor2 = hsl(adjustColor(hue2, i), color2SL[0], color2SL[1]);
+  const newColor3 = hsl(adjustColor(hue3, i), color3SL[0], color3SL[1]);
+  let result = farNearer.replace(/COLOR_1/g, newColor1)
+  result = result.replace(/COLOR_2/g, newColor2)
+  result = result.replace(/COLOR_3/g, newColor3)
+  fs.writeFileSync(`far_nearer${i}.svg`, result)
+}
